37484c35c3492eba5510f452732fa324c681a5ec
2551250 <2551250@students.wits.ac.za>
2024-04-24 14:46:06 +0200

    Possible fix to deployment

--- app.js
+++ app.js
@@ -10,11 +10,23 @@ app.get('/', async (req, res) => {
   res.send('API reached');
 });
 
-app.get('/Employee', async (req, res) =>{
-  ret = await query(`SELECT * FROM dbo.EMPLOYEE;`)
+app.get('/Employee', async (req, res) => {
+  ret = await query(`SELECT * FROM dbo.EMPLOYEE;`);
   res.send(ret);
 });
 
+app.get('/Project', async (req, res) => {
+  ret = await query(`SELECT * FROM dbo.PROJECT;`);
+  res.send(ret);
+});
+
+app.get('/Employee/Project/:id', async (req, res) => {
+  const id = req.params.id;
+  ret = await query(
+    `SELECT DISTINCT PROJECT_NAME, DESCRIPTION, ESTIMATED_TIME FROM dbo.PROJECT INNER JOIN dbo.EMPLOYEE_PROJECT ON (dbo.PROJECT.PROJECT_ID = dbo.EMPLOYEE_PROJECT.PROJECT_ID) INNER JOIN dbo.EMPLOYEE ON (dbo.EMPLOYEE.EMPLOYEE_ID = dbo.EMPLOYEE_PROJECT.EMPLOYEE_ID) WHERE dbo.EMPLOYEE.EMPLOYEE_ID = ${id};`
+  );
+  res.send(ret);
+});
 
 const config = {
   user: 'workwise-backend-server-admin', // better stored in an app setting such as process.env.DB_USER
